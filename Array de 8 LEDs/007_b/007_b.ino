883ea4d42014fd03ec5395c5b961062036394383
sulama2021 <81186908+sulama2021@users.noreply.github.com>
2021-03-27 15:26:41 +0100

    Create 007_b.ino

---
+++ Array de 8 LEDs/007_b/007_b.ino
@@ -0,0 +1,37 @@
+/**********************************************************************************
+**                                                                               **
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int ledPin[] = {5, 6, 7, 8};   // donar nom en un array als pins 5, 6, 7 i 8
+int ledNum = 4;                      // definir variable de número de leds
+int pausa = 1000;                    // definir variable de temps en ms
+
+//********** Setup ****************************************************************
+void setup()
+{
+  for(int i = 0; i < ledNum; i++) // definir els pins 5, 6, 7 i 8 com sortides
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  for(int i = 0; i < 16; i++)     // mostrar del 0 al 15
+  {
+    for(int j = 0; j < ledNum ; j++)   // actualitzar estat leds per mostrar número
+    { 
+      digitalWrite(ledPin[j], bitRead(i, j));
+    }
+    delay(pausa);                 // es queden leds pausa ms en aquest estat
+  }
+}
+
+//********** Funcions *************************************************************
